b806ea7a35110adcc42280316f9ab6b2c93a7cea
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-09-18 21:51:47 +0300

    add somtings push in to project

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
+
   return (
     <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://t4.ftcdn.net/jpg/04/31/89/39/360_F_431893913_vpGxndMDShXEwC44i7CzM4amNEEWuZmg.jpg" />
+  <div className="container mx-auto flex px-5 py-14 items-center justify-center flex-col">
+    {/* <img  className="lg:w-3/4 md:w-3/4 w-2/3 mb-10 object-cover object-center rounded"alt="hero" src="https://t4.ftcdn.net/jpg/04/31/89/39/360_F_431893913_vpGxndMDShXEwC44i7CzM4amNEEWuZmg.jpg" 
+    /> */}
+    <Image  className="lg:w-2/2 md:w-2/3 w-2/2 mb-10 object-cover object-center"
+      src="/image/profile.jpg"
+      width={500}
+      height={50}
+      alt="Picture of the author"
+    />
     <div className="text-center lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
       <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import Hero from './components/Hero'
+import Content from './components/Content'
+import Feature from './components/Feature'
 
 export default function Home() {
   return (
    <main>
     <Hero />
+    <Content />
+    <Feature />
    </main>
   )
 }
